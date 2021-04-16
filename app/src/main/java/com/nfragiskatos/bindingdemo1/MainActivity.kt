8a92253e45aaa88b071274f5d6a96a76e0555c00
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-15 20:21:30 -0700

    Constructed Data Binding object in MainActivity.kt, and used it instead of findViewById to utilize the views.

--- app/src/main/java/com/nfragiskatos/bindingdemo1/MainActivity.kt
+++ app/src/main/java/com/nfragiskatos/bindingdemo1/MainActivity.kt
@@ -1,27 +1,22 @@
 package com.nfragiskatos.bindingdemo1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.nfragiskatos.bindingdemo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.submit_button)
-        button.setOnClickListener {
-            displayGreeting()
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.apply {
+            submitButton.setOnClickListener {
+                greetingTextView.text = "Hello! ${nameEditText.text}"
+            }
         }
-    }
-
-    private fun displayGreeting() {
-        val messageView = findViewById<TextView>(R.id.greeting_text_view)
-        val nameText = findViewById<EditText>(R.id.name_edit_text)
-
-        val message = "Hello! "+ nameText.text
-        messageView.text = message
     }
 }
