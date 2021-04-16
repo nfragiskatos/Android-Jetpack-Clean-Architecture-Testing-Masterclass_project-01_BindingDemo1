db3a0b70cfdc7ba80f4964a8ba40cda29c8c3d28
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-15 20:55:22 -0700

    Contrived example showing how you can use Data Binding with objects, and map the object fields to different views.

--- app/src/main/java/com/nfragiskatos/bindingdemo1/MainActivity.kt
+++ app/src/main/java/com/nfragiskatos/bindingdemo1/MainActivity.kt
@@ -8,15 +8,28 @@ import com.nfragiskatos.bindingdemo1.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val students: MutableList<Student> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initData()
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.apply {
-            submitButton.setOnClickListener {
-                greetingTextView.text = "Hello! ${nameEditText.text}"
-            }
+        binding.submitButton.setOnClickListener {
+            binding.student = students.random()
         }
+    }
+
+    private fun initData() {
+        students.add(Student(1, "Bruce Wayne", "batman@batcave.com"))
+        students.add(Student(2, "Clark Kent", "superman@justiceleague.com"))
+        students.add(Student(3, "Wayne Brady", "waynebrady@whoseline.com"))
+        students.add(Student(4, "Barry Allen", "flash@justiceleague.com"))
+        students.add(Student(5, "Tony Start", "ironman@avengers.com"))
+        students.add(Student(6, "Bruce Banner", "hulk@avengers.com"))
+        students.add(Student(7, "Thor", "thor@avengers.com"))
+        students.add(Student(8, "Carol Danvers", "captainmarvel@avengers.com"))
+        students.add(Student(9, "Peter Parker", "spiderman@avengers.com"))
+        students.add(Student(10, "Stanley Tucci", "stanleytucci@prada.com"))
     }
 }
---
+++ app/src/main/java/com/nfragiskatos/bindingdemo1/Student.kt
@@ -0,0 +1,7 @@
+package com.nfragiskatos.bindingdemo1
+
+data class Student(
+    var id: Int,
+    var name: String,
+    var email: String
+)
