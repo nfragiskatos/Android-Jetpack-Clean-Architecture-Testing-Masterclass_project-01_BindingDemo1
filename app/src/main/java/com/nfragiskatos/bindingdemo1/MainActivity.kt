39e68c01ec770aaf57857c56e5712f84b288845b
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-17 06:23:10 -0700

    - Created MainActivityViewModel.kt to be the ViewModel for MainActivity.kt in order to preserve data between configuration changes (e.g. screen rotation), since ViewModel survives the complete lifecycle of the activity (from onCreate -> onDestroy)

--- app/src/main/java/com/nfragiskatos/bindingdemo1/MainActivity.kt
+++ app/src/main/java/com/nfragiskatos/bindingdemo1/MainActivity.kt
@@ -3,21 +3,23 @@ package com.nfragiskatos.bindingdemo1
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.nfragiskatos.bindingdemo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val students: MutableList<Student> = ArrayList()
-    private var count = 0;
+    private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initData()
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        binding.countTextView.text = viewModel.getCurrentCount().toString()
         binding.submitButton.setOnClickListener {
-            binding.countTextView.text = (++count).toString()
+            binding.countTextView.text = viewModel.getUpdatedCount().toString()
         }
     }
 
---
+++ app/src/main/java/com/nfragiskatos/bindingdemo1/MainActivityViewModel.kt
@@ -0,0 +1,11 @@
+package com.nfragiskatos.bindingdemo1
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+    private var count = 0;
+
+    fun getCurrentCount() : Int = count
+
+    fun getUpdatedCount() : Int = ++count
+}
